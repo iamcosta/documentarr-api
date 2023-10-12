1ee463f1fe727ebbe811a8b3eb9186065cd34e4a
Iam Barroso <iambarrosodacosta@live.com>
2023-10-12 13:13:33 -0400

    commit inicial

---
+++ src/test/java/dev/kitnet/documentarapi/DocumentarApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.kitnet.documentarapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocumentarApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
